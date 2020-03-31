6969e72db039703b12d0df0383c25b9d450dc50f
Yuriy <yurariba@gmail.com>
2020-03-31 23:36:07 +0300

    get heroes asynchronosly

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,8 +22,13 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
