eeead5cfd2764b3689558fa2db4a9ab850c733d6
Yuriy <yurariba@gmail.com>
2020-03-31 22:35:52 +0300

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Super Kek'},
+  {id: 2, name: 'Super Pek'},
+  {id: 3, name: 'Igor Nikolaev'},
+  {id: 4, name: 'Vipjem Za Lubov'},
+  {id: 5, name: 'Kak Prekrasny Sejchas Tvoi Glaza'},
+  {id: 6, name: 'Stul'},
+  {id: 7, name: 'Stol'},
+  {id: 8, name: 'Devyat'},
+  {id: 9, name: 'Dumbledor'},
+  {id: 10, name: 'Semyon'}
+];
